3095ed4e9031baafd751adbcf775fcee45c2519c
Tasneem Gad <tasneemgad38@gmail.com>
2026-08-07 15:50:03 +0300

    add translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-
+import {provideTranslateService} from "@ngx-translate/core";
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
+    provideRouter(routes), provideClientHydration(),
+     provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'ar',
+      lang: 'ar'
+    })
   ]
 };
